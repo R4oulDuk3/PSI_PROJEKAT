99585e7b213f05d1b7a80dedf43a56677057f198
Savke57 <ssavke52@gmail.com>
2022-04-21 16:39:27 +0200

    Izmenjeni kuponi page, dodati events
    Nekoliko known bugs, awaiting fix

--- Faza 2/Prototip - HTML/clientSide/cupons/script.js
+++ Faza 2/Prototip - HTML/clientSide/cupons/script.js
@@ -7,3 +7,26 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll=() =>{
     navbar.classList.remove('active');
 }
+
+function change(){
+    var myEle = document.getElementById("bought-invis");
+    if (myEle) {
+        document.getElementById("purch-vis").id= "purch-invis";
+        document.getElementById("swap").textContent = " Kupovina kupona ";
+        document.getElementById("bought-invis").id="bought-vis"
+    }
+    else{
+    document.getElementById("bought-vis").id = "bought-invis";
+    document.getElementById("swap").textContent = " Moji kuponi ";
+    document.getElementById("purch-invis").id = "purch-vis";
+    }
+}
+
+
+function openForm() {
+    document.getElementById("popForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("popForm").style.display = "none";
+  }
