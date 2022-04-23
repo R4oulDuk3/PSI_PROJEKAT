92e1d38483043e603bb5636df794990d3fa259ad
Gavrilo Vojteški <gavrilovojteski37@gmail.com>
2022-04-24 01:30:32 +0200

    dodat js za ucitavanje artikala

--- Faza 2/Prototip - HTML (novi)/Admin/inventar/ucitaj_artikle.js
+++ Faza 2/Prototip - HTML (novi)/Admin/inventar/ucitaj_artikle.js
@@ -10,9 +10,11 @@ var artikliUNedostatku = [
 
     },
 ]
+
+
 var artikli =[
     {
-        name: 'Amstel',
+        name: 'Lasko',
         kolicina : '10',
 
         sifraProizvoda: '2123232323232',
@@ -21,38 +23,73 @@ var artikli =[
 
     },
 ]
-
+var modalOpen = false
+var ignoreDoc=false
 
 $(document).ready(function (){
+        
     let grid1 = $('#porudzbenice-grid')
     let grid2 = $('#artikli-grid')
-    
-    function popuni(grid,podaci){
-        podaci.forEach((artikal)=>{
-            
-        })
-    }
-    data.forEach((zurka)=>{
-        let karta = $('<div></div>').addClass('card');
-        let dogadjaj = $("<div></div>").addClass('card-content');
-        let naslov = $("<h2></h2>").addClass('card-title').append(zurka.name);
-        let opis = $("<p></p>").addClass('card-body').append(zurka.desc);
-        let dugme = $("<a></a>").attr('href','#').addClass('button-card').addClass('modal-btn-del').append('Obrisi dogadjaj');
-        dogadjaj.append(naslov);
-        dogadjaj.append(opis);
-        dogadjaj.append(dugme);
-        karta.append(dogadjaj);
-        karta.css('background-image',"url("+zurka.imgName+")");
-        var img = new Image;
-        img.src = zurka.imgName
-        var height = img.height;
-        console.log(height)
-        //karta.css('padding-top', height+"px");
-        grid.append(karta)
-        //grid.append($('<div></div>')); 
-    })
+    popuni(grid1,artikliUNedostatku)
+    popuni(grid2,artikli)
+    $(document).click(function(event) { 
+        console.log('close')
+        if(ignoreDoc){
+            ignoreDoc=false;
+            return
+        }
+        console.log(modalOpen)
+        var $target = $(event.target);
+        if(modalOpen && !$target.closest('#delete-modal-content').length) {
+            console.log('closing')
+          $('#delete-modal').css('display','none')
+            modalOpen=false;
+        }        
+      });
 
+
+    const modal_del = document.querySelector('#delete-modal');
+    document.querySelectorAll('.modal-btn-del').forEach(item => {
+        item.addEventListener('click', ()=>{
+
+        });
+    });
 })
+function openModal(placeholder){
+    modalOpen=true
+    ignoreDoc=true
+    console.log(placeholder)
+    $('#delete-modal').css('display','block')
+    $('#modal-del-input').attr('placeholder',placeholder)
+}
+function closeModal(){
+    $('modal').each(()=>{
+        this.css('display','none')
+    })
+}
 
-
-
+// dodati da se na window klik svi modali zatvore
+function popuni(grid,podaci){
+    console.log("check")
+    podaci.forEach((artikal)=>{
+        let karta = $('<div></div>').addClass('card-single');
+        let naslovniDiv = $('<div></div>').addClass('naslov')
+        let naslov = $('<h1></h1>').append(artikal.name)
+        let buttonsDiv = $('<div></div>').addClass('buttons')
+        let buttonChange = $('<button class="sm-button modal-btn"><span class="las la-exchange-alt"></span></button>')
+        let buttonDelete = $('<button class="sm-button modal-btn-del"><span class="las la-times"></span></button>')
+        buttonDelete.on('click',()=>{openModal(artikal.name)})
+        buttonsDiv.append(buttonChange)
+        buttonsDiv.append(buttonDelete)
+        naslovniDiv.append(naslov)
+        naslovniDiv.append(buttonsDiv)
+        let table = $('<table></table>')
+        table.append($('<tr></tr>').append($('<td></td>').append('Kolicina')).append($('<td></td>').append(artikal.kolicina)))
+        table.append($('<tr></tr>').append($('<td></td>').append('Sifra')).append($('<td></td>').append(artikal.sifra)))
+        table.append($('<tr></tr>').append($('<td></td>').append('Nabavna cena')).append($('<td></td>').append(artikal.nabavnaCena+" RSD")))
+        table.append($('<tr></tr>').append($('<td></td>').append('Sifra dobavljaca')).append($('<td></td>').append(artikal.sifraDobavljaca)))
+        karta.append(naslovniDiv)
+        karta.append(table)
+        grid.append(karta)
+    })
+}
