66e646d1867c8a54deefe08dbed8502306ed5a8e
Marina00 <djokovicmarina@gmail.com>
2022-05-30 10:08:13 +0200

    ss

---
+++ Konobar/UnosRezervacije/UnosRezervacije.js
@@ -0,0 +1,21 @@
+var dogadjaji=[
+    {
+        id: 1,
+        name: 'Amstel',
+        kolicina : '10',
+        nedostatakLimit: '10',  //
+        sifraProizvoda: '2123232323232',
+        nabavnaCena: '140',
+        sifraDobavljaca: '1231231232131'
+
+    },{
+        id: 2,
+        name: 'Niksicko',
+        kolicina : '10',
+       nedostatakLimit: '10',
+        sifraProizvoda: '2123232323232',
+        nabavnaCena: '140',
+        sifraDobavljaca: '1231231232131'
+
+    }
+]
